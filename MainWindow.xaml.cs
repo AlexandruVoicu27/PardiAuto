163dc46bda89915a695567295a4c3a7a77c6a82b
AlexandruVoicu27 <alecs.voicu@outlook.com>
2026-05-16 17:59:33 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoPartsShop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Aplicatia porneste direct cu pagina de Login
+            MainFrame.Navigate(new LoginPage());
+        }
+    }
+
+}
