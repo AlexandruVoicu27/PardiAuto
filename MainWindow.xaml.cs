50f9a2e482622611006cbeeddd3abe2ee1a0c744
Alexandru Voicu <alecs.voicu@outlook.com>
2026-06-06 13:09:27 +0300

    Wire admin modules into main navigation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AutoPartsShop
 {
@@ -16,13 +9,12 @@ namespace AutoPartsShop
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Utilizator utilizatorCurent;
+        private Utilizator? utilizatorCurent;
 
         public MainWindow()
         {
             InitializeComponent();
             MainFrame.Navigated += MainFrame_Navigated;
-            // Aplicatia porneste direct cu pagina de Login
             MainFrame.Navigate(new LoginPage());
         }
 
@@ -30,17 +22,26 @@ namespace AutoPartsShop
         {
             InitializeComponent();
             utilizatorCurent = utilizator;
-
+            ConfigureazaMeniuDupaRol();
         }
 
         public void SetUtilizatorCurent(Utilizator utilizator)
         {
             utilizatorCurent = utilizator;
             UserSalutText.Text = $"Salut, {utilizatorCurent.Nume}!";
-            MainFrame.Navigate(new MainPage(utilizatorCurent));
+            ConfigureazaMeniuDupaRol();
+
+            if (utilizatorCurent.Rol == RolUtilizator.Administrator)
+            {
+                MainFrame.Navigate(new AdminDashboardPage(utilizatorCurent));
+            }
+            else
+            {
+                MainFrame.Navigate(new MainPage(utilizatorCurent));
+            }
         }
 
-        private void MainFrame_Navigated(object sender, NavigationEventArgs e) //functie sa nu am buton vizibil cand sunt pe pagina de login sau creare cont
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
         {
             if (MainFrame.Content is LoginPage || MainFrame.Content is CreateAccountPage)
             {
@@ -48,49 +49,121 @@ namespace AutoPartsShop
                 UserSalutText.Visibility = Visibility.Collapsed;
                 ProfilUtilizator.Visibility = Visibility.Collapsed;
                 BtnAcasa.Visibility = Visibility.Collapsed;
+                BtnDashboardAdmin.Visibility = Visibility.Collapsed;
+                BtnAdminUtilizatori.Visibility = Visibility.Collapsed;
+                BtnAdminComenzi.Visibility = Visibility.Collapsed;
+                BtnPlatiFacturi.Visibility = Visibility.Collapsed;
                 BtnPieseAuto.Visibility = Visibility.Collapsed;
                 BtnComandaMea.Visibility = Visibility.Collapsed;
-
             }
             else
             {
                 BtnDeconectare.Visibility = Visibility.Visible;
                 UserSalutText.Visibility = Visibility.Visible;
                 ProfilUtilizator.Visibility = Visibility.Visible;
-                BtnAcasa.Visibility = Visibility.Visible;
-                BtnPieseAuto.Visibility = Visibility.Visible;
-                BtnComandaMea.Visibility = Visibility.Visible;
+                ConfigureazaMeniuDupaRol();
             }
         }
-           
+
+        private void ConfigureazaMeniuDupaRol()
+        {
+            if (utilizatorCurent == null)
+            {
+                return;
+            }
+
+            bool esteAdmin = utilizatorCurent.Rol == RolUtilizator.Administrator;
+            bool esteStaff = utilizatorCurent.Rol == RolUtilizator.Administrator || utilizatorCurent.Rol == RolUtilizator.Angajat;
+            bool esteClient = utilizatorCurent.Rol == RolUtilizator.Client;
+
+            BtnAcasa.Visibility = Visibility.Visible;
+            BtnPieseAuto.Visibility = Visibility.Visible;
+            BtnComandaMea.Visibility = esteClient ? Visibility.Visible : Visibility.Collapsed;
+
+            BtnDashboardAdmin.Visibility = esteAdmin ? Visibility.Visible : Visibility.Collapsed;
+            BtnAdminUtilizatori.Visibility = esteAdmin ? Visibility.Visible : Visibility.Collapsed;
+            BtnAdminComenzi.Visibility = esteStaff ? Visibility.Visible : Visibility.Collapsed;
+            BtnPlatiFacturi.Visibility = esteStaff ? Visibility.Visible : Visibility.Collapsed;
+        }
+
         private void BtnDeconectare_Click(object sender, RoutedEventArgs e)
         {
-            // Navigam inapoi la pagina de login
+            utilizatorCurent = null;
             MainFrame.Navigate(new LoginPage());
         }
 
         private void BtnAcasa_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new MainPage(utilizatorCurent));
+            if (utilizatorCurent == null)
+            {
+                MainFrame.Navigate(new LoginPage());
+                return;
+            }
+
+            if (utilizatorCurent.Rol == RolUtilizator.Administrator)
+            {
+                MainFrame.Navigate(new AdminDashboardPage(utilizatorCurent));
+            }
+            else
+            {
+                MainFrame.Navigate(new MainPage(utilizatorCurent));
+            }
+        }
+
+        private void BtnDashboardAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            if (utilizatorCurent != null)
+            {
+                MainFrame.Navigate(new AdminDashboardPage(utilizatorCurent));
+            }
+        }
+
+        private void BtnAdminUtilizatori_Click(object sender, RoutedEventArgs e)
+        {
+            if (utilizatorCurent != null)
+            {
+                MainFrame.Navigate(new ConfigurareAngajatiPage(utilizatorCurent));
+            }
+        }
+
+        private void BtnAdminComenzi_Click(object sender, RoutedEventArgs e)
+        {
+            if (utilizatorCurent != null)
+            {
+                MainFrame.Navigate(new AdministrareComenziPage(utilizatorCurent));
+            }
+        }
+
+        private void BtnPlatiFacturi_Click(object sender, RoutedEventArgs e)
+        {
+            if (utilizatorCurent != null)
+            {
+                MainFrame.Navigate(new PlatiFacturiPage(utilizatorCurent));
+            }
         }
 
         private void BtnPieseAuto_Click(object sender, RoutedEventArgs e)
         {
-            MainFrame.Navigate(new PieseAutoPage(utilizatorCurent, "Toate"));
+            if (utilizatorCurent != null)
+            {
+                MainFrame.Navigate(new PieseAutoPage(utilizatorCurent, "Toate"));
+            }
         }
 
         private void BtnComandaMea_Click(object sender, RoutedEventArgs e)
         {
-            
-            MainFrame.Navigate(new ComandaMeaPage(utilizatorCurent));
+            if (utilizatorCurent != null)
+            {
+                MainFrame.Navigate(new ComandaMeaPage(utilizatorCurent));
+            }
         }
 
         private void ProfilUtilizator_Click(object sender, RoutedEventArgs e)
         {
-           
-            MainFrame.Navigate(new ProfilUtilizator(utilizatorCurent));
-
+            if (utilizatorCurent != null)
+            {
+                MainFrame.Navigate(new ProfilUtilizator(utilizatorCurent));
+            }
         }
-
     }
 }
